f87fcee670e9e23be7142d7dc7139d86c64269cc
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-09 01:49:52 +0100

    refactor - add bootstrap.bundle.min.js in main.tsx

--- dev/src/main.tsx
+++ dev/src/main.tsx
@@ -5,9 +5,12 @@ import ReactDOM from "react-dom/client";
 // App root component
 import App from "./App.tsx";
 
-// Styles
+// Custom styles
 import "./index.css";
+
+// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
