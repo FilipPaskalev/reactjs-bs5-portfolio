c4a1a90342ba56cbda59b6d8687df2db1723dd82
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-12 00:09:57 +0100

    refactor-  render content dynamically

--- src/components/cards/experience/experienceCard.tsx
+++ src/components/cards/experience/experienceCard.tsx
@@ -1,25 +1,39 @@
-// @flow
-import * as React from "react";
-type Props = {};
-const experienceCard = (props: Props) => {
+type Props = {
+  position: string;
+  company: {
+    name: string;
+    website: string;
+  };
+  description: string[];
+  startDate: string;
+  endDate: string;
+};
+
+const ExperienceCard = ({
+  position,
+  company,
+  description,
+  startDate,
+  endDate,
+}: Props) => {
   return (
-    <section className="d-flex flex-column flex-md-row justify-content-between mb-5">
-      <hgroup className="flex-grow-1">
-        <h3 className="mb-0">Senior Web Developer</h3>
-        <h4 className="subheading mb-3">Intelitec Solutions</h4>
-        <p>
-          Bring to the table win-win survival strategies to ensure proactive
-          domination. At the end of the day, going forward, a new normal that
-          has evolved from generation X is on the runway heading towards a
-          streamlined cloud solution. User generated content in real-time will
-          have multiple touchpoints for offshoring.
-        </p>
-      </hgroup>
-      <footer className="flex-shrink-0">
-        <span className="text-primary">March 2013 - Present</span>
-      </footer>
-    </section>
+    <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+      <div className="flex-grow-1">
+        <h3 className="mb-0">{position}</h3>
+        <div className="subheading mb-3">{company.name}</div>
+        {description.map((desc, index) => (
+          <p key={index} className="my-0 py-0">
+            {desc}
+          </p>
+        ))}
+      </div>
+      <div className="flex-shrink-0 pt-3">
+        <span className="text-primary">
+          {startDate} - {endDate}
+        </span>
+      </div>
+    </div>
   );
 };
 
-export default experienceCard;
+export default ExperienceCard;
