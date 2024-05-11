455bdda13ce775bf1dfadca4a683c1440a720f07
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-12 00:13:45 +0100

    add semantic HTML

--- src/components/cards/experience/experienceCard.tsx
+++ src/components/cards/experience/experienceCard.tsx
@@ -17,22 +17,22 @@ const ExperienceCard = ({
   endDate,
 }: Props) => {
   return (
-    <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+    <article className="d-flex flex-column flex-md-row justify-content-between mb-5">
       <div className="flex-grow-1">
         <h3 className="mb-0">{position}</h3>
-        <div className="subheading mb-3">{company.name}</div>
+        <h4 className="subheading mb-3">{company.name}</h4>
         {description.map((desc, index) => (
           <p key={index} className="my-0 py-0">
             {desc}
           </p>
         ))}
       </div>
-      <div className="flex-shrink-0 pt-3">
+      <footer className="flex-shrink-0 pt-3">
         <span className="text-primary">
           {startDate} - {endDate}
         </span>
-      </div>
-    </div>
+      </footer>
+    </article>
   );
 };
 
--- src/sections/experience/ExperienceSection.tsx
+++ src/sections/experience/ExperienceSection.tsx
@@ -4,12 +4,12 @@ import ExperienceCard from "../../components/cards/experience/experienceCard";
 const ExperienceSection = () => {
   return (
     <section className="resume-section" id="experience">
-      <div className="resume-section-content">
+      <section className="resume-section-content">
         <h2 className="mb-5">Experience</h2>
         {EXPERIENCE_DATA.map((experience, index) => (
           <ExperienceCard key={index} {...experience} />
         ))}
-      </div>
+      </section>
     </section>
   );
 };
