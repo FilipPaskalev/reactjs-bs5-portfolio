c24842efff0cc00f5697d0501b57aafb3ec9214a
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-11 23:30:16 +0100

    add link to institution in EducationCard.tsx

--- src/components/cards/education/EducationCard.tsx
+++ src/components/cards/education/EducationCard.tsx
@@ -1,5 +1,13 @@
+import { IoCalendarNumberOutline } from "react-icons/io5";
+import { FaTrophy } from "react-icons/fa6";
+import { FaShareAlt } from "react-icons/fa";
+import { CiLink } from "react-icons/ci";
+
 type Props = {
-  institution: string;
+  institution: {
+    name: string;
+    website: string;
+  };
   degree: string;
   fieldOfStudy: string;
   description: string[];
@@ -20,20 +28,28 @@ const EducationCard = ({
   return (
     <section className="d-flex flex-column flex-md-row justify-content-between mb-5">
       <hgroup className="flex-grow-1">
-        <h3 className="mb-0">{institution}</h3>
-        <h4 className="mb-3 text-muted">{degree}</h4>
-        <h5 className="subheading">{fieldOfStudy}</h5>
+        <h3 className="mb-0">
+          {institution.name}
+          <a href={institution.website} target="_blank">
+            <CiLink />
+          </a>
+        </h3>
+        <h4 className="mb-1 text-muted">{degree}</h4>
+        <h5 className="subheading mb-3">{fieldOfStudy}</h5>
         {description.map((desc, index) => (
           <p key={index} className="my-0">
             {desc}
           </p>
         ))}
-        <p className="my-3">{grade}</p>
+        <p className="my-3">
+          <FaTrophy className="text-primary" /> {grade}
+        </p>
       </hgroup>
       <footer className="flex-shrink-0">
-        <span className="text-primary">
-          {startDate} - {endDate}
-        </span>
+        <p>
+          <IoCalendarNumberOutline className="text-primary" /> {startDate} -{" "}
+          {endDate}
+        </p>
       </footer>
     </section>
   );
