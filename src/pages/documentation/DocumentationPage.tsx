563f3f82092765505c79d8f1a8563a32ec51bc8d
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-14 00:19:18 +0100

    add coming soon page to pages that are not ready

--- src/pages/documentation/DocumentationPage.tsx
+++ src/pages/documentation/DocumentationPage.tsx
@@ -1,10 +1,13 @@
+import ComingSoonPage from '../comingSoon/ComingSoonPage'
+
 const DocumentationPage = () => {
   return (
-    <section className='resume-section' id='project-documentation'>
-      <div className='resume-section-content'>
-        <h2>Documentation of this project</h2>
-      </div>
-    </section>
+    // <section className='resume-section' id='project-documentation'>
+    //   <div className='resume-section-content'>
+    //     <h2>Documentation of this project</h2>
+    //   </div>
+    // </section>
+    <ComingSoonPage />
   )
 }
 
--- src/pages/projects/ProjectsPage.tsx
+++ src/pages/projects/ProjectsPage.tsx
@@ -1,10 +1,13 @@
+import ComingSoonPage from '../comingSoon/ComingSoonPage'
+
 const ProjectsPage = () => {
   return (
-    <section className='resume-section' id='project-documentation'>
-      <div className='resume-section-content'>
-        <h2>My Projects page</h2>
-      </div>
-    </section>
+    // <section className='resume-section' id='project-documentation'>
+    //   <div className='resume-section-content'>
+    //     <h2>My Projects page</h2>
+    //   </div>
+    // </section>
+    <ComingSoonPage />
   )
 }
 
