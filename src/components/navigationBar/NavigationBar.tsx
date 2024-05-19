17099da4bdd64aa40289e90218a362801126fe46
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-19 23:31:26 +0100

    refactor: image Image from react-bootstrap

--- src/components/navigationBar/NavigationBar.tsx
+++ src/components/navigationBar/NavigationBar.tsx
@@ -1,32 +1,32 @@
-import { useState, useEffect, useRef } from 'react'
-import USER_DATA from '../../data/user.json'
-import AVATAR from '../../assets/png/avatar-hi.png'
-import { Container, Navbar, Nav } from 'react-bootstrap'
+import { useState, useEffect, useRef } from "react";
+import USER_DATA from "../../data/user.json";
+import AVATAR from "../../assets/png/avatar-hi.png";
+import { Container, Navbar, Nav, Image } from "react-bootstrap";
 import {
   FaTelegram,
   FaWhatsapp,
   FaViber,
   FaBars,
   FaTimes
-} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import EPagesPaths from '../../enum/EPagesPaths'
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+import EPagesPaths from "../../enum/EPagesPaths";
 
 const NavigationBar = () => {
-  const [menuOpen, setMenuOpen] = useState(false)
-  const navbarRef = useRef<HTMLDivElement>(null)
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navbarRef = useRef<HTMLDivElement>(null);
 
   const scrollToTop = () => {
-    window.scrollTo(0, 0)
-  }
+    window.scrollTo(0, 0);
+  };
 
   const handleToggle = () => {
-    setMenuOpen(!menuOpen)
-  }
+    setMenuOpen(!menuOpen);
+  };
 
   const handleClose = () => {
-    setMenuOpen(false)
-  }
+    setMenuOpen(false);
+  };
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -34,196 +34,183 @@ const NavigationBar = () => {
         navbarRef.current &&
         !navbarRef.current.contains(event.target as Node)
       ) {
-        setMenuOpen(false)
+        setMenuOpen(false);
       }
-    }
+    };
 
-    document.addEventListener('mousedown', handleClickOutside)
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [])
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <Navbar
-      expand='lg'
-      variant='dark'
-      bg='primary'
-      fixed='top'
-      id='sideNav'
+      expand="lg"
+      variant="dark"
+      bg="primary"
+      fixed="top"
+      id="sideNav"
       expanded={menuOpen}
-      ref={navbarRef}
-    >
+      ref={navbarRef}>
       <Navbar.Brand href={EPagesPaths.HOME}>
-        <b className='d-block d-lg-none'>
+        <b className="d-block d-lg-none">
           {USER_DATA.firstName} {USER_DATA.lastName}
         </b>
-        <Container className='d-none d-lg-block'>
-          <img
-            className='img-fluid img-profile rounded-circle mx-auto mb-2'
+        <Container className="d-none d-lg-block">
+          <Image
+            className="img-fluid img-profile rounded-circle mx-auto mb-2"
             src={AVATAR}
-            alt='profile avatar'
+            alt="profile avatar"
           />
         </Container>
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls='navbarResponsive' onClick={handleToggle}>
+      <Navbar.Toggle aria-controls="navbarResponsive" onClick={handleToggle}>
         {menuOpen ? <FaTimes /> : <FaBars />}
       </Navbar.Toggle>
-      <Navbar.Collapse id='navbarResponsive'>
-        <Nav className='navbar-nav'>
+      <Navbar.Collapse id="navbarResponsive">
+        <Nav className="navbar-nav">
           <Nav.Item>
             <Link
-              className='nav-link nav-item'
+              className="nav-link nav-item"
               to={EPagesPaths.HOME}
               onClick={() => {
-                scrollToTop()
-                handleClose()
-              }}
-            >
+                scrollToTop();
+                handleClose();
+              }}>
               About Me
             </Link>
           </Nav.Item>
           <Nav.Item>
             <Link
-              className='nav-link'
+              className="nav-link"
               to={EPagesPaths.EXPERIENCE}
               onClick={() => {
-                scrollToTop()
-                handleClose()
-              }}
-            >
+                scrollToTop();
+                handleClose();
+              }}>
               Experience
             </Link>
           </Nav.Item>
           <Nav.Item>
             <Link
-              className='nav-link'
+              className="nav-link"
               to={EPagesPaths.EDUCATION}
               onClick={() => {
-                scrollToTop()
-                handleClose()
-              }}
-            >
+                scrollToTop();
+                handleClose();
+              }}>
               Education
             </Link>
           </Nav.Item>
           <Nav.Item>
             <Link
-              className='nav-link'
+              className="nav-link"
               to={EPagesPaths.MY_SKILLS}
               onClick={() => {
-                scrollToTop()
-                handleClose()
-              }}
-            >
+                scrollToTop();
+                handleClose();
+              }}>
               My Skills
             </Link>
           </Nav.Item>
           <Nav.Item>
             <Link
-              className='nav-link'
+              className="nav-link"
               to={EPagesPaths.INTERESTS}
               onClick={() => {
-                scrollToTop()
-                handleClose()
-              }}
-            >
+                scrollToTop();
+                handleClose();
+              }}>
               Interests
             </Link>
           </Nav.Item>
           <Nav.Item>
             <Link
-              className='nav-link'
+              className="nav-link"
               to={EPagesPaths.AWARDS}
               onClick={() => {
-                scrollToTop()
-                handleClose()
-              }}
-            >
+                scrollToTop();
+                handleClose();
+              }}>
               Awards
             </Link>
           </Nav.Item>
           <Nav.Item>
             <Link
-              className='nav-link'
+              className="nav-link"
               to={EPagesPaths.CERTIFICATIONS}
               onClick={() => {
-                scrollToTop()
-                handleClose()
-              }}
-            >
+                scrollToTop();
+                handleClose();
+              }}>
               Certifications
             </Link>
           </Nav.Item>
           <Nav.Item>
             <Link
-              className='nav-link'
+              className="nav-link"
               to={EPagesPaths.PROJECTS}
               onClick={() => {
-                scrollToTop()
-                handleClose()
-              }}
-            >
+                scrollToTop();
+                handleClose();
+              }}>
               Projects
             </Link>
           </Nav.Item>
 
-          <hr className='m-0 px-2 text-light' />
+          <hr className="m-0 px-2 text-light" />
 
-          <Nav.Item id='about-this-project-link'>
+          <Nav.Item id="about-this-project-link">
             <Link
-              className='nav-link'
+              className="nav-link"
               to={EPagesPaths.DOCUMENTATION}
               onClick={() => {
-                scrollToTop()
-                handleClose()
-              }}
-            >
+                scrollToTop();
+                handleClose();
+              }}>
               Documentation
             </Link>
           </Nav.Item>
-          <Container className='social-icons'>
+          <Container className="social-icons">
             <a
-              data-bs-toggle='tooltip'
-              data-bs-placement='top'
-              className='social-icon'
+              data-bs-toggle="tooltip"
+              data-bs-placement="top"
+              className="social-icon"
               title={USER_DATA.social.telegram.title}
               href={USER_DATA.social.telegram.href}
-              target='_blank'
-              rel='noopener noreferrer'
-              onClick={handleClose}
-            >
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={handleClose}>
               <FaTelegram />
             </a>
             <a
-              data-bs-toggle='tooltip'
-              data-bs-placement='top'
-              className='social-icon'
+              data-bs-toggle="tooltip"
+              data-bs-placement="top"
+              className="social-icon"
               title={USER_DATA.social.whatsapp.title}
               href={USER_DATA.social.whatsapp.href}
-              target='_blank'
-              rel='noopener noreferrer'
-              onClick={handleClose}
-            >
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={handleClose}>
               <FaWhatsapp />
             </a>
             <a
-              data-bs-toggle='tooltip'
-              data-bs-placement='top'
-              className='social-icon'
+              data-bs-toggle="tooltip"
+              data-bs-placement="top"
+              className="social-icon"
               title={USER_DATA.social.viber.title}
               href={USER_DATA.social.viber.href}
-              target='_blank'
-              rel='noopener noreferrer'
-              onClick={handleClose}
-            >
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={handleClose}>
               <FaViber />
             </a>
           </Container>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-  )
-}
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
