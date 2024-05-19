6853f9a85d1af300f0ab7829a4b394de7210db7d
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-05-19 23:21:57 +0100

    Add TODo comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,24 @@
 // React entry point
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // App root component
-import App from './App.tsx'
+import App from "./App.tsx";
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 
 // Overriding Bootstrap styles with custom styles
-import './scss/styles.scss'
+import "./scss/styles.scss";
 
 // TODO: implement https://lingui.dev/ for i18n
+// TODO: implement AXIOS for API calls & get all data from API, not from local JSON files
+// TODO: implement local storage for saving user data
+// TODO: implement placeholder for all components -> images, text, cards, etc.
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
